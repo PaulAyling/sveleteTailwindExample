36c33255812696d4a503466d45256c041de6c05b
PaulAyling <jimmyyukka@gmail.com>
2021-12-27 07:57:33 +0100

    installed tailwind and page layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import '../app.css';
+</script>
+
+<main>
+    template
+  <div class="max-w-4xl mx-auto py-8 bg-red-500">
+      <slot />
+  </div>
+</main>
---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  // add this section
+  purge: [
+    './src/**/*.html',
+    './src/**/*.svelte'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
