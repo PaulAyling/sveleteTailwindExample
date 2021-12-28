71927e0126868f314a35b2059e5efa071d98189c
PaulAyling <jimmyyukka@gmail.com>
2021-12-28 18:42:43 +0100

    got a second store to work

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,10 @@
     import Basic from '../components/tutorials/1_UpdateStore/basic.svelte'
     //Tutorial 1
     import RenderUpdateState from '../components/tutorials/1_UpdateStore/renderUpdateState.svelte';
+    import DDrop from '../components/tutorials/2_DragNDrop/dDrop.svelte'
+    import DDropStore from '../components/tutorials/3_DragNDropStore/dDropStore.svelte'
 </script>
-<Basic/>
-<RenderUpdateState/>
+<!-- <Basic/> -->
+<!-- <RenderUpdateState/> -->
+<!-- <DDrop/> -->
+<DDropStore/>
--- src/stores.js
+++ src/stores.js
@@ -20,3 +20,22 @@ export const flowers = writable([
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
   },
 ])
+
+export const baskets = writable([
+  {
+    "name": "Basket 1",
+    "items": ["Orange", "Pineapple"]
+  },
+  {
+    "name": "Basket 2",
+    "items": ["Banana", "Apple"]
+  },
+  {
+    "name": "Basket 3",
+    "items": ["GrapeFruit"]
+  },
+  {
+    "name": "Basket 4",
+    "items": ["peach"]
+  }
+  ])
