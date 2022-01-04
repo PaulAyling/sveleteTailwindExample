0b42938229108c2a0a64f70ab662f4490e9af62c
PaulAyling <jimmyyukka@gmail.com>
2022-01-04 11:00:01 +0100

    created tutorial showing a custom store

---
+++ src/components/tutorials/7_customStore/customStore.svelte
@@ -0,0 +1,30 @@
+<script>
+    import {flowers} from './myStore'
+    import Card from '../../layout/card.svelte'
+    import Child from './child.svelte'
+    import InputBox from '../../layout/input.svelte' 
+
+    
+
+    flowers.subscribe(value => {
+		console.log('Store value:', value)
+	});
+</script>
+<h1>An example showing read/write on a custom store in a custom location</h1>
+<div>
+    <div class='bg-green-100 p-4'>
+        <h1>Loop store contents</h1>
+        <p>This shows a store being loaded in and used and modified </p>
+        <p>As the store is modified the updated store is ouptutted to console</p>
+    </div>
+    <ul>
+        {#each $flowers as flower}
+        <Card>
+            <li> {flower.title}</li> 
+        </Card>
+        <InputBox bind:title={flower.title}/>
+        {/each}
+
+    </ul>
+    <Child/>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,11 +7,13 @@
     // import DDropEdit from '../components/tutorials/4_DragnDropEditable/dDropEdit.svelte';
 // import DDropStore from '../components/tutorials/3_DragNDropStore/dDropStore.svelte'
     // import StoreView from '../components/tutorials/5_logStoreState/inputpm.svelte'
-    import Inputpmp from '../components/tutorials/6_inputpmap/6_inputpmap.svelte'
+    // import Inputpmp from '../components/tutorials/6_inputpmap/6_inputpmap.svelte'
+    import CustomStore from '../components/tutorials/7_customStore/customStore.svelte'
 </script>
 <!-- <Basic/> -->
 <!-- <RenderUpdateState/> -->
 <!-- <DDrop/> -->
 <!-- <DDropStore/> -->
 <!-- <DDropEdit/> -->
-<Inputpmp/>
+<!-- <Inputpmp/> -->
+<CustomStore/>
