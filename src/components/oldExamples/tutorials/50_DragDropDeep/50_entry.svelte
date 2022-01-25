12cc30a7e4b57c777cdbe089b14d4d0f2da0c840
PaulAyling <jimmyyukka@gmail.com>
2022-01-25 14:58:44 +0100

    changed data

--- src/components/oldExamples/tutorials/50_DragDropDeep/50_entry.svelte
+++ src/components/oldExamples/tutorials/50_DragDropDeep/50_entry.svelte
@@ -1,41 +1,28 @@
 <script>
-	import Draggable from './Draggable.svelte'	
-	import {cardLayout, pointerOverCard} from './store.js'
-	import {add, remove} from './utilities.js'
-
-// 	const startingId = $cardLayout[1].apexCardId
-const startingId = 0 // until I figure out how to do this in the store
-
- cardLayout.subscribe(value => {
-		console.log('STORE:',value )
-	})
+	import Draggable from './Draggable.svelte';
+	import { cardLayout, pointerOverCard } from './store.js';
+	import { add, remove } from './utilities.js';
+	cardLayout.subscribe((value) => {
+		console.log('STORE:', value);
+	});
+	const startingId = 0;
 </script>
-<h1>
-	Based on DRAGDROP.svelte with different data
-</h1>
 
+<Draggable
+	cardId={startingId}
+	bind:pointerOverCard={$pointerOverCard}
+	bind:cardLayout={$cardLayout}
+/>
 
-<Draggable 
-cardId={startingId} 
-bind:pointerOverCard={$pointerOverCard}  						bind:cardLayout={$cardLayout} />
-
-<button class="delete" on:click={() => remove(3,0)}>
-	Delete
-</button>
-
-
-<button class="add" on:click={() => add(2,9)}>
-	Add
-</button>
-
-
+<button class="delete" on:click={() => remove(3, 0)}> Delete </button>
+<button class="add" on:click={() => add(2, 9)}> Add </button>
 
 <style>
-	h1{
+	h1 {
 		text-transform: uppercase;
-		font-size:1em
+		font-size: 1em;
 	}
-	.delete{
-		background:red;
+	.delete {
+		background: red;
 	}
 </style>
--- src/components/oldExamples/tutorials/50_DragDropDeep/store.js
+++ src/components/oldExamples/tutorials/50_DragDropDeep/store.js
@@ -1,9 +1,9 @@
 import { writable } from 'svelte/store'
 export const cardLayout = writable({
-    0:{'id':0,'children':[1,2,3]},
-    1:{'id':1,'children':[4,5]},
+    0:{'id':0,'children':[1]},
+    1:{'id':1,'children':[1,2,3]},
     2:{'id':2,'children':[]},
-    3:{'id':3,'children':[]},
+    3:{'id':3,'children':[4,5]},
     4:{'id':4,'children':[7]},
     5:{'id':5,'children':[6]},
     6:{'id':6,'children':[]},
